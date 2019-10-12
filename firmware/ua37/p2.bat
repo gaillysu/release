13e6b2323fbff9271f663ddf5217567e9ec72083
su@ce.com.hk <su@ce.com.hk>
2019-10-13 00:04:11 +0800

    no message

---
+++ firmware/ua37/p2.bat
@@ -0,0 +1,8 @@
+nvscmd  -usb 253531  erase
+nvscmd  -usb 253531  burn  ua-37qt_v12.xuv
+nvscmd  -usb 253531  verify  ua-37qt_v12.xuv
+pause
+
+
+
+
---
+++ firmware/ua42/p1.bat
@@ -0,0 +1,7 @@
+nvscmd  -usb 334296  erase
+nvscmd  -usb 334296  burn  ua-42qt-v_1009.xuv
+nvscmd  -usb 334296  verify  ua-42qt-v_1009.xuv
+pause
+
+
+
